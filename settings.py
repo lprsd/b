792506ab1e3047a782f576ea830bb67f0edfdff8
Lakshman Prasad <ub@ub-ThinkPad-SL500.(none)>
2010-11-02 14:45:30 +0530

    Add south and keep the dbfile within the branch!

--- settings.py
+++ settings.py
@@ -94,7 +94,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
         
-    #'south',
+    'south',
     'django_extensions',
     
     
