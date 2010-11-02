cdd18eb9a1cadaca0cbe2cb48905e80611e54bdd
Lakshman Prasad <ub@ub-ThinkPad-SL500.(none)>
2010-11-02 14:28:37 +0530

    First working version that has the db (needs extensions, south and taggit in env)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class BaseBlogModel(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField()
+    author = models.ForeignKey(User)
+
+    class Meta:
+        abstract = True
+        
+class Post(BaseBlogModel):
+    title = models.CharField(max_length=80)
+    
+class Comment(BaseBlogModel):
+    post = models.ForeignKey(Post)
+    
--- settings.py
+++ settings.py
@@ -93,8 +93,14 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+        
     #'south',
-    'extensions',
+    'django_extensions',
+    
+    
+    'taggit',
+    'blog',
+    
 )
 
 
